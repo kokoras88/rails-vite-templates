afefd2c6e477c410d859ecabb8dcb93c8fc83db5
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-04-17 23:32:35 +0200

    Fixed vite config lint

--- minimal.rb
+++ minimal.rb
@@ -111,17 +111,17 @@ after_bundle do
   # Vite Config
   ########################################
   vite_config_ts = <<~JS
-    import {defineConfig} from 'vite'
+    import { defineConfig } from "vite"
     import FullReload from "vite-plugin-full-reload"
-    import RubyPlugin from 'vite-plugin-ruby'
-    import StimulusHMR from 'vite-plugin-stimulus-hmr'
+    import RubyPlugin from "vite-plugin-ruby"
+    import StimulusHMR from "vite-plugin-stimulus-hmr"
     
     export default defineConfig({
       clearScreen: false,
       plugins: [
         RubyPlugin(), 
         StimulusHMR(), 
-        FullReload(["config/routes.rb", "app/views/**/*"], {delay: 200}),
+        FullReload(["config/routes.rb", "app/views/**/*"], { delay: 200 }),
       ],
     })
   JS
